6934a69b76ba4f9f897b13304ded47dbcc8a5bad
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-06-09 19:35:41 -0400

    Unique Ids
    Used ShortId object to give each of my list items a unique id

--- src/list.js
+++ src/list.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import shortid from "shortid";
 var List = ({ listArr }) => {
   return (
     <ul>
       {listArr.map(info => (
-        <li>
+        <li key={shortid.generate()}>
           {info} {info.counter}
         </li>
       ))}
